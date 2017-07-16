ef4dc161d6146a144ea0b8a22649dedc1507c34f
dany1468 <dany1468@gmail.com>
2017-07-17 07:25:35 +0900

    Change repository description
    command:
    $ terraform plan
    $ terraform apply

--- main.tf
+++ main.tf
@@ -4,4 +4,8 @@ provider "github" {
 
 resource "github_repository" "example" {
   name        = "breakable_todo_app"
+  description = "Update with Terraform"
+  has_wiki = true
+  has_issues = true
+  has_downloads = true
 }
