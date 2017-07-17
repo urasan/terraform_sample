814dc47a91323285ad6c9e556439bad590036e56
dany1468 <dany1468@gmail.com>
2017-07-17 21:12:16 +0900

    Reflect tfstate info in main.tf

--- main.tf
+++ main.tf
@@ -2,3 +2,10 @@ provider "github" {
   organization = "urasan"
 }
 
+resource "github_repository" "urasan_api" {
+  name        = "api"
+  description = "裏"
+  has_wiki = true
+  has_issues = true
+  has_downloads = true
+}
