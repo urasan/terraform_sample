83391b44ab78ded1ba472f958a0b0da8e60080fd
dany1468 <dany1468@gmail.com>
2017-07-17 21:09:57 +0900

    Import api repository state belonging to urasan org

--- main.tf
+++ main.tf
@@ -1,11 +1,4 @@
 provider "github" {
-  organization = "dany1468"
+  organization = "urasan"
 }
 
-resource "github_repository" "example" {
-  name        = "breakable_todo_app"
-  description = "Update with Terraform"
-  has_wiki = true
-  has_issues = true
-  has_downloads = true
-}
